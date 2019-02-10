76a76733b128bbdd4de44c1b5dfb637d911601a9
Derek Pyle <derekpyle@gmail.com>
2019-02-10 14:11:07 -0500

    Change app.js listen

--- app.js
+++ app.js
@@ -23,37 +23,7 @@ app.get("/projects", function(req, res){
 app.get("/contact", function(req, res){
    res.render("contact");
 })
-app.post("/contact/send", function(req, res) {
-   var smtpTransport = nodemailer.createTransport({
-       service: 'Gmail', 
-       auth: {
-         user: 'youremail@domain.com',
-         pass: process.env.GMAILPW
-       }
-   });
-    
-   var mailOptions = {
-       from: 'Your Name <youremail@domain.com>',
-       to: 'youremail@domain.com',
-       replyTo: req.body.email,
-       subject: 'Website Submission',
-       text: 'You have a submission with the following details... Name: '+ req.body.name + ' Phone: ' + req.body.phone + ' Email: ' + req.body.email + ' Message: ' + req.body.message,
-       html: '<h3>You have a submission with the following details...</h3><ul><li>Name: ' + req.body.name + ' </li><li>Phone: ' + req.body.phone + ' </li><li>Email: ' + req.body.email + ' </li></ul><p>Message: <br/><br/>     ' + req.body.message + ' </p>'
-   };
-   
-   smtpTransport.sendMail(mailOptions, function(err, info){
-     if(err) {
-       req.flash("error", "Something went wrong");
-       console.log(err);
-       res.redirect("/contact");
-     } else {
-       req.flash("success", "Your email has been sent, we will respond within 24 hours.");
-       console.log("Message sent " + info.response);
-       res.redirect("/campgrounds");
-       
-     }
-   });
-   
-});
 
-app.listen(3000, () => console.log(`Example app listening on port ${3000}!`))
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("The YelpCamp Server Has Started!");
+});
